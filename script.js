9f7d38baffc811b606e607cffca98bf93954c78f
ally-glenday <66145089+ally-glenday@users.noreply.github.com>
2023-10-23 15:38:49 +0900

    Centred menu

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function toggleHamburgerMenu(){
     const hamburgerMenuIcon = document.getElementById("hamburger-toggle-ul");
+    const navHamburgerMenu = document.getElementById("nav-flexbox-container");
     hamburgerMenuIcon.classList.toggle("visible");
+    navHamburgerMenu.classList.toggle("hamburger-on-click");
 }
