9107f21802bafa9a14227aaef7d0042bfaeacf1a
ally-glenday <66145089+ally-glenday@users.noreply.github.com>
2023-10-24 11:56:53 +0900

    Display grid

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function toggleHamburgerMenu(){
     const hamburgerMenuIcon = document.getElementById("hamburger-toggle-ul");
-    const navHamburgerMenu = document.getElementById("nav-flexbox-container");
-    hamburgerMenuIcon.classList.toggle("display-flex");
+    const navHamburgerMenu = document.getElementById("nav-grid-container");
+    hamburgerMenuIcon.classList.toggle("display-grid");
     navHamburgerMenu.classList.toggle("hamburger-on-click");
 }
