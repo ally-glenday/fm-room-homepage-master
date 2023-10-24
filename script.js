5728b2ab1c1ea437e4674784265295628dfcccae
ally-glenday <66145089+ally-glenday@users.noreply.github.com>
2023-10-24 10:47:21 +0900

    Nav bar working nicely

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function toggleHamburgerMenu(){
     const hamburgerMenuIcon = document.getElementById("hamburger-toggle-ul");
     const navHamburgerMenu = document.getElementById("nav-flexbox-container");
-    hamburgerMenuIcon.classList.toggle("visible");
+    hamburgerMenuIcon.classList.toggle("display-flex");
     navHamburgerMenu.classList.toggle("hamburger-on-click");
 }
