d7793d91b3be86dc47cd16e5cc13ccea9cc8c7be
ally-glenday <66145089+ally-glenday@users.noreply.github.com>
2023-10-25 11:08:21 +0900

    Nav bar working!

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function toggleHamburgerMenu(){
     const hamburgerMenuIcon = document.getElementById("hamburger-toggle-ul");
     const navHamburgerMenu = document.getElementById("nav-grid-container");
+    const toggleIcon = document.getElementById("toggle-icon");
     hamburgerMenuIcon.classList.toggle("display-grid");
     navHamburgerMenu.classList.toggle("hamburger-on-click");
+    toggleIcon.classList.toggle("close-icon");
 }
