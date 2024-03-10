3214a26f72917da84a5309a3918edc3c6a3cc161
ally-glenday <66145089+ally-glenday@users.noreply.github.com>
2024-03-10 11:08:55 +0900

    Working fine (before adding carosel)

--- script.js
+++ script.js
@@ -14,3 +14,9 @@ if (window.screen.width >= 550 && window.screen.height >= 550) {
     document.getElementById("main-section").appendChild(slideContainer);
   }
 
+
+/* 
+
+1) onclick, display next / prev slide 
+
+2)  create a list of 3 slides, with an index number 0, 1, 2, then go + or - to slide between each slide.
